526ce98d32609dba5aa6ec510c4758c7ae72a3b2
Senna Bhany Majid <sennakanjhiwa@gmail.com>
2024-10-24 19:44:34 +0700

    commit pertama

---
+++ scripts/app.js
@@ -0,0 +1,55 @@
+// If Else, else if
+// const uchiha = "Itachi";
+
+// if (uchiha === "Madara") {
+//     console.log("Uchiha Madara");
+// } else if (uchiha === "Itachi") {
+//     console.log("Uchiha Itachi");
+// } else if (uchiha === "Izumi") {
+//     console.log("Uchiha Izumi");
+// } else {
+//     console.log("Al-Khurasan Al-Kufi");
+// }
+
+
+// Switch Case
+// const king = "Sulaiman";
+
+// switch (king.toLowerCase()) {
+//     case "dzulkarnain":
+//         console.log("King Dzulkarnain");
+//         break;
+//     case "Daud":
+//         console.log("King Daud");
+//         break;
+//     case "sulaiman":
+//         console.log("King Sulaiman");
+//         break;
+//     default:
+//         console.log("King Itacheat");
+// }
+
+// For Loop
+// const cats = ['red', 'black', 'white'];
+
+// for (let i = 0; i < cats.length; i++) {
+//     console.log(`Cat color ${cats[i]}`);
+// }
+
+// While Loop
+// const kings = ["Itachi", "Madara", "Indra", "Ashura"];
+// let i = 2;
+
+// while (i < kings.length) {
+//     console.log(`Uchiha ${kings[i]}`);
+//     i++;
+// }
+
+// Do While
+// const kings = ["Itachi", "Madara", "Hashirama", "Ashura"];
+// let i = 0;
+
+// do {
+//     console.log(`Senju ${kings[i]}`);
+//     i++;
+// } while (i < kings.length);
